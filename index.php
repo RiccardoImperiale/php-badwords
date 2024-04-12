7554c8095ad2b1308ac89374541910e3d189700a
RiccardoImperiale <riccardoimperiale.dev@gmail.com>
2024-04-12 14:19:22 +0200

    print the paragraph and its length on the screen

--- index.php
+++ index.php
@@ -9,15 +9,22 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Php Badwords</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
 </head>
 
 <body>
-    <form action="./script.php" method="get">
-        <input type="text" placeholder="write here">
-        <input type="text" placeholder="word to be censored...">
-        <button type="submit">Censore word</button>
+    <form action="./script.php" method="get" class="container w-50 mt-5">
+        <div class="mb-3">
+            <label for="paragraph" class="form-label">Paragraph:</label>
+            <textarea name="paragraph" id="paragraph" class="form-control" rows="5" placeholder="Write a sentence..."></textarea>
+        </div>
+        <div class="mb-3">
+            <label for="word" class="form-label">Word to be censored:</label>
+            <input name="word" type="text" class="form-control" id="word" placeholder="Type the word you want to censor...">
+        </div>
+        <button type="submit" class="btn btn-primary">Censor</button>
     </form>
-
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 </body>
 
 </html>
--- script.php
+++ script.php
@@ -1,2 +1,26 @@
 <?php
-var_dump("hi from script.php");
+
+var_dump($_GET);
+$paragraph = $_GET['paragraph'];
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Results</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+</head>
+
+<body>
+    <div class="container mt-5">
+        <h3>The paragraph contains <?= strlen($paragraph) ?> characters</h3>
+        <p><?= $paragraph ?></p>
+    </div>
+
+</body>
+
+</html>
