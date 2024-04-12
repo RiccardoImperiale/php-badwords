a3fabd55a1e7a4083fddc88555be145dcb5dde41
RiccardoImperiale <riccardoimperiale.dev@gmail.com>
2024-04-12 13:58:09 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Php Badwords</title>
+</head>
+
+<body>
+    <form action="./script.php" method="get">
+        <input type="text" placeholder="write here">
+        <input type="text" placeholder="word to be censored...">
+        <button type="submit">Censore word</button>
+    </form>
+
+</body>
+
+</html>
